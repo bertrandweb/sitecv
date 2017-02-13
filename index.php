749f040ab8409ceb927eaf9de4b03b3336796b76
bertrandweb <feuille.bertrand@orange.fr>
2017-02-13 18:32:44 +0100

    Début de la réalisation du site :
    -ajout de plusieurs plugin (animate, hover, normalized)
    -mise en forme du menu principal
    -ajout de l'image de présentation du site

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
-/**
- * Created by PhpStorm.
- * User: Bertrand Feuille
- * Date: 13/02/2017
- * Time: 13:40
- */
-
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
         <title>Bertrand Feuille</title>
-        <link rel="stylesheet" href="/css/style_principal.css">
+
+        <link rel="stylesheet" href="./css/hover.css">
+        <link rel="stylesheet" href="./css/animate.css">
+        <link rel="stylesheet" href="./css/normalized.css">
+        <link rel="stylesheet" href="./css/style.css">
     </head>
     <body>
         <div class="page">
@@ -18,9 +15,36 @@
             <div id="loading" class="loading"></div>
             <!-- preloader end  -->
         <header>
+          <div class="nom">
+            <h1>Bertrand Feuille Web développeur</h1>
+          </div>
             <div id="header" class="navbar_header">
+              <ul>
+                <li><a class="hvr-underline-reveal href" href="#">Accueil</a></li>
+                <li><a class="hvr-underline-reveal href" href="#">Présentation</a></li>
+                <li><a class="hvr-underline-reveal href" href="#">Coméptences</a></li>
+                <li><a class="hvr-underline-reveal href" href="#">Réalisations</a></li>
+                <li><a class="hvr-underline-reveal href" href="#">Contact</a></li>
+              </ul>
             </div>
         </header>
+        <!-- Image de fond -->
+        <div class="hero">
+          <div class="filtre_hero">
+            <div class="bienvenue">
+              <h2>Bienvenue</h2>
+            </div>
+          </div>
+        </div>
+        <!-- fin image de fond -->
+        <!-- bloc compétence ou autre -->
+        <div class="competence">
+          <div class="intro_competence">
+            <h6>Bienvenue sur mon site</h6>
+            <h3>Recrutez un web développeur de talent ! </h3>
+            <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem</p>
+          </div>
+        </div>
         </div>
     </body>
 </html>
