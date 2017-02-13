ab5789ecb9b0f44df6930f8dde29dca52a781914
bertrandweb <feuille.bertrand@orange.fr>
2017-02-13 13:57:12 +0100

    Mise en place des fichiers / Début du projet /

---
+++ index.php
@@ -0,0 +1,25 @@
+/**
+ * Created by PhpStorm.
+ * User: Bertrand Feuille
+ * Date: 13/02/2017
+ * Time: 13:40
+ */
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Bertrand Feuille</title>
+    </head>
+    <body>
+        <div class="page">
+            <!-- preloader start  -->
+            <div id="loading" class="loading"></div>
+            <!-- preloader end  -->
+        <header>
+            <div id="header" class="navbar_header">
+            </div>
+        </header>
+        </div>
+    </body>
+</html>
