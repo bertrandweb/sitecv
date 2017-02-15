3d183024f5dfdba488a4024b3f5b9682ab762331
bertrandweb <feuille.bertrand@orange.fr>
2017-02-15 15:00:33 +0100

    Début de la réalisation du site :
    - ajout de html
    - ainsi que le css toujours en sass

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <header>
           <div class="nom">
             <!-- Image + logo ici -->
-            <h1>Bertrand Feuille Web développeur</h1>
+            <h3>Bertrand Feuille Web développeur</h3>
           </div>
             <div id="header" class="navbar_header">
               <ul>
@@ -34,21 +34,96 @@
           <div class="filtre_hero">
             <div class="bienvenue">
               <h1>Site CV de Bertrand Feuille</h1>
-              <h2>Webmaster sur Rouen</h2>
+              <h2><span class="span_word">Webmaster</span> sur Rouen</h2>
               <p>Passionné du Web et des nouvelles technologies depuis des années, je me présente aujourd’hui à vous en tant que <span>Webmaster</span>. Un métier qui me permet de mettre à profit mes compétences de développeur web, de graphiste et d’intégrateur.</p>
-              <a href="#"><button class="hvr-bounce-in" type="button" name="">Contactez moi</button></a>
+              <a class="button" href="#"><span>Contact</span></a>
             </div>
           </div>
         </div>
         <!-- fin image de fond -->
         <!-- bloc compétence ou autre -->
-        <div class="competence">
+        <section class="competence">
           <div class="intro_competence">
-            <h6>Bienvenue sur mon site</h6>
-            <h3>Recrutez un web développeur de talent ! </h3>
-            <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem</p>
+            <h3>Bienvenue sur mon site</h3>
+            <h4>Recrutez un web développeur de talent ! </h4>
           </div>
+          <div class="wrapper">
+            <div class="competence_bloc">
+              <img src="" alt="">
+              <a href="#">Développement</a>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="competence_bloc">
+              <img src="" alt="">
+              <a href="#">Graphisme</a>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="competence_bloc">
+              <img src="" alt="">
+              <a href="#">Responsive Design</a>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="competence_bloc">
+              <img src="" alt="">
+              <a href="#">Développement</a>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+        </section>
+        <section class="realisation">
+          <div class="intro_realisation">
+            <h3>Mes réalisations</h3>
+          </div>
+          <div class="wrapper">
+            <div class="realisation_bloc">
+              <img src="" alt="">
+              <a href="#">Développement</a>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="competence_bloc">
+              <img src="" alt="">
+              <a href="#">Développement</a>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="competence_bloc">
+              <img src="" alt="">
+              <a href="#">Développement</a>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+        </section>
+        <!-- debut image background -->
+        <div class="hero2">
+        <div class="filtre_hero2">
         </div>
+        </div>
+        <!-- fin image background -->
+        <section class="contact">
+          <div class="intro_contact">
+              <h3>Contactez moi</h3>
+          </div>
+          <div class="wrapper">
+            <div class="formulaire">
+              <h4>Demande de renseignements</h4>
+              <p>Pour tous renseignemtnes sur <span class="span_word"> mon profil</span> ou pour me <span class="span_word">rencontrer</span>. N'hésitez pas à me contacter.</p>
+              <form class="" action="index.html" method="post">
+                <input type="text" name="" value="" placeholder="Nom">
+                <input type="email" name="" value="" placeholder="Adresse e-mail">
+                <textarea name="name" rows="8" cols="80">Message</textarea>
+                <input type="submit" name="" value="Envoi">
+              </form>
+            </div>
+            <div class="coordonnees">
+              <h4>Mes coordonnées</h4>
+              <p>Bertrand Feuille</p>
+              <p><strong>Tel : </strong>06 26 49 48 49</p>
+              <p><strong>email : </strong>contact@bertrandfeuille.fr</p>
+              <p><strong>email : </strong><a href="www.bertrandfeuille.fr">bertrandfeuille.fr</a></p>
+            </div>
+          </div>
+        </section>
+        <footer>
+        </footer>
         </div>
     </body>
 </html>
