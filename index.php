78ded50295516251b43294eb2dd94a90c7c0d3fe
bertrandweb <feuille.bertrand@orange.fr>
2017-02-16 17:05:57 +0100

    Début de la réalisation du site :
    - ajout de html
    - ainsi que le css toujours en sass

--- index.php
+++ index.php
@@ -2,13 +2,20 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Bertrand Feuille</title>
+        <title>Bertrand Feuille | Webdéveloppeur-Webmaster</title>
 
+        <!-- CSS -->
         <link rel="stylesheet" href="./font/font-awesome/css/font-awesome.min.css">
         <link rel="stylesheet" href="./css/hover.css">
         <link rel="stylesheet" href="./css/animate.css">
         <link rel="stylesheet" href="./css/normalized.css">
         <link rel="stylesheet" href="./css/style.css">
+
+        <!-- FAVICON -->
+        <link rel="icon" type="image/png" href="./image/logofavicon.png" />
+        <!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="./image/favicon.ico" /><![endif]-->
+
+
     </head>
     <body>
         <div class="page">
@@ -18,15 +25,17 @@
         <header>
           <div class="nom">
             <!-- Image + logo ici -->
-            <img src="./image/logo_site_cv.png" alt="logo de bertrand feuille">
+            <img src="./image/logo2_site_cv.png" alt="logo de bertrand feuille">
+            <p>Bertrand Feuille</p>
+
           </div>
             <nav id="header" class="navbar_header">
               <ul>
-                <li><a class="hvr-underline-reveal href" href="#">Accueil</a></li>
-                <li><a class="hvr-underline-reveal href" href="#">Présentation</a></li>
-                <li><a class="hvr-underline-reveal href" href="#">Compétences</a></li>
-                <li><a class="hvr-underline-reveal href" href="#">Réalisations</a></li>
-                <li><a class="hvr-underline-reveal href" href="#">Contact</a></li>
+                <li><a class="hvr-underline-reveal href" href="#header">Accueil</a></li>
+                <li><a class="hvr-underline-reveal href" href="#presentation">Présentation</a></li>
+                <li><a class="hvr-underline-reveal href" href="#competence">Compétences</a></li>
+                <li><a class="hvr-underline-reveal href" href="#realisation">Réalisations</a></li>
+                <li><a class="hvr-underline-reveal href" href="#formulaire">Contact</a></li>
               </ul>
             </nav>
         </header>
@@ -37,52 +46,46 @@
               <h1>Site CV de Bertrand Feuille</h1>
               <h2><span class="span_word">Webmaster</span> sur Rouen</h2>
               <p>Passionné du Web et des nouvelles technologies depuis des années, je me présente aujourd’hui à vous en tant que <span>Webmaster</span>. Un métier qui me permet de mettre à profit mes compétences de développeur web, de graphiste et d’intégrateur.</p>
-              <a class="button" href="#"><span>Contact</span></a>
+              <a class="button" href="#formulaire"><span>Contact</span></a>
             </div>
           </div>
         </div>
         <!-- fin image de fond -->
-        <!-- bloc compétence ou autre -->
-        <section class="competence">
-          <div class="intro_competence">
-            <h3>Bienvenue sur mon site</h3>
+        <section id=presentation class="presentation_bloc">
+          <div class="wrapper">
+            <div class="photo"><img src="./image/logo2_site_cv.png" alt=""></div>
+            <div class="presentation">
+              <h3>Présentation</h3>
+              <p><strong>Après un BTS en informatique m’ayant formé en programmation, réseaux, développement web, j’ai choisi de compléter mon profil technique avec des compétences en graphisme, communication web et référencement.</strong></p>
+              <p>La Formation Qualifiante Webmaster du lycée Saliège m’a donné les outils pour concevoir,
+              organiser, intégrer et animer un site web. J’ai pu mettre en application lors de mes différentes
+              expériences professionnelles en tant que webdesigner- intégrateur au sein d’une équipes. Je perfectionne chaque jour ces compétences que ce soit pour mes projets personnels ou pour les projets de mes clients en tant qu’auto-entrepreneur.</p>
+            </div>
+
           </div>
+        </section>
+        <!-- bloc compétence -->
+        <section id="competence" class="competence"><h3>Compétences</h3>
           <div class="wrapper">
             <div class="competence_bloc">
               <p><a href="#">Développement</a></p>
-              <p><i class="fa fa-code fa-5x"></i></p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p><i class="fa fa-code fa-4x"></i></p>
+              <p>Utilisant les langages HTML 5, CSS3 et PHP depuis plus de 5 ans, je maîtrise également Javascript et jQuery ce qui me confère les connaissances nécessaires pour modifier les CMS rencontré ou m’adapter aux nouvelles techniques des projets réalisés</p>
             </div>
             <div class="competence_bloc">
               <p><a href="#">Graphisme</a></p>
-              <p><i class="fa fa-picture-o fa-5x"></i></p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p><i class="fa fa-picture-o fa-4x"></i></p>
+              <p>Maitrisant Photoshop et Illustrator je réalise les visuels dont j’ai besoin, les maquettes et charte graphique de site web. J’utilise ma créativité pour créer des logos, flyers, et retouches d’image.</p>
             </div>
             <div class="competence_bloc">
               <p><a href="#">Responsive Design</a></p>
-              <p><i class="fa fa-mobile fa-5x"></i></p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p><i class="fa fa-mobile fa-4x"></i></p>
+              <p>Le trafic web sur smartphones et tablettes n’étant plus négligeable. J’utilise mes compétences en intégration et design pour réaliser des sites qui s’adaptent à toutes les tailles de support.</p>
             </div>
             <div class="competence_bloc">
               <p><a href="#">Communication</a></p>
-              <p><i class="fa  fa-comments-o fa-5x"></i></p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-          </div>
-        </section>
-        <section class="realisation">
-          <div class="intro_realisation">
-            <h3>Mes réalisations</h3>
-          </div>
-          <div class="wrapper">
-            <div class="realisation_bloc">
-              <a href="http://ikedori.bertrandfeuille.fr"><img src="./image/realisation_ikedori.png" alt="logo de ikedori"></a>
-            </div>
-            <div class="realisation_bloc">
-              <a href="http://www.influenceursduweb.org/"><img src="./image/realisation_influenceur.png" alt="logo des influenceurs du web"></a>
-            </div>
-            <div class="realisation_bloc">
-              <a href="http://www.webmaster-formation.fr/r"><img src="./image/realisation_webmaster.png" alt="logo de ikedori"></a>
+              <p><i class="fa  fa-comments-o fa-4x"></i></p>
+              <p>Afin d’assurer la visibilité d’un site internet, je sais mettre en place une stratégie de communication et d’acquisition de trafic en utilisant différents leviers tels que les réseaux sociaux et le référencement naturel.</p>
             </div>
           </div>
         </section>
@@ -92,34 +95,71 @@
         </div>
         </div>
         <!-- fin image background -->
-        <section class="contact">
-          <div class="intro_contact">
-              <h3>Contactez moi</h3>
-              
+        <section id="realisation" class="realisation">
+          <div class="intro_realisation">
+            <h3>Mes réalisations</h3>
           </div>
           <div class="wrapper">
-            <div class="formulaire">
-              <h4>Demande de renseignements</h4>
-              <p>Pour tous renseignements sur <span class="span_word"> mon profil</span> ou pour me <span class="span_word">rencontrer</span>. N'hésitez pas à me contacter.</p>
-              <form class="" action="index.html" method="post">
-                <p><input type="text" name="" value="" placeholder="Nom">
-                <input type="email" name="" value="" placeholder="Adresse e-mail"></p>
-                <textarea name="name" rows="8" cols="80" placeholder="Message"></textarea>
-                <p><input type="submit" name="" value="Envoi"></p>
-              </form>
+            <div class="realisation_bloc">
+              <a href="http://ikedori.bertrandfeuille.fr" target=_blank><img src="./image/realisation_ikedori.png" alt="logo de ikedori"></a>
             </div>
-            <div class="coordonnees">
-              <h4>Mes coordonnées</h4>
-              <p>Bertrand Feuille</p>
-              <p><strong>Tel : </strong>06 26 49 48 49</p>
-              <p><strong>email : </strong>contact@bertrandfeuille.fr</p>
-              <p><strong>email : </strong><a href="http://www.bertrandfeuille.fr">bertrandfeuille.fr</a></p>
+            <div class="realisation_bloc">
+              <a href="http://www.influenceursduweb.org/" target=_blank><img src="./image/realisation_influenceur.png" alt="logo des influenceurs du web"></a>
+            </div>
+            <div class="realisation_bloc">
+              <a href="http://www.webmaster-formation.fr/r" target=_blank><img src="./image/realisation_webmaster.png" alt="logo de ikedori"></a>
             </div>
           </div>
         </section>
+        <!-- debut image background -->
+        <div class="hero2">
+        <div class="filtre_hero2">
+        </div>
+        </div>
+        <!-- fin image background -->
+        <!-- Section contatact -->
+        <section id="formulaire" class="formulaire_contact">
+          <h3>Me contacter</h3>
+            <form class="" action="index.html" method="post">
+              <p><input type="text" name="" value="" placeholder="Nom">
+              <input type="email" name="" value="" placeholder="Adresse e-mail"></p>
+              <textarea name="name" rows="8" cols="80" placeholder="Message"></textarea>
+              <p><input type="submit" name="" value="Envoi"></p>
+            </form>
+        </section>
+        <section>
+            <div class="wrapper_contact">
+              <div class="contact_adress">
+                <p><i class="fa  fa-phone fa-2x"></i></p>
+                <h4>Téléphone</h4>
+                <h5>06 26 49 48 49</h5>
+              </div>
+              <div class="contact_adress">
+                <p><i class="fa  fa-download fa-2x"></i></p>
+                <h4>CV</h4>
+                <h5><a href="#">Télécharger CV</a></h5>
+              </div>
+              <div class="contact_adress">
+                <p><i class="fa  fa-envelope-o fa-2x"></i></p>
+                <h4>Email</h4>
+                <h5>contact@bertrandfeuille.fr</h5>
+              </div>
+            </div>
+            <div class="social_network">
+              <p>
+                <a href="https://twitter.com/Bertrand_Web" target=_blank><i class="fa  fa-twitter fa-2x"></i></a>
+                <a href="https://www.linkedin.com/in/bertrand-feuille-6ba79aaa?authType=NAME_SEARCH&authToken=_4O6&locale=fr_FR&srchid=3901246541487256692129&srchindex=1&srchtotal=1&trk=vsrp_people_res_name&trkInfo=VSRPsearchId%3A3901246541487256692129%2CVSRPtargetId%3A390124654%2CVSRPcmpt%3Aprimary%2CVSRPnm%3Atrue%2CauthType%3ANAME_SEARCH" target=_blank><i class="fa  fa-linkedin-square fa-2x"></i></a>
+              </p>
+            </div>
+        </section>
+
         <footer>
           <p>Copiright 2017 - <a href="#">Mentions légales</a></p>
         </footer>
         </div>
+
+        <!-- JS -->
+        <script type="text/javascript" src="./js/jquery-3.1.1.js"></script>
+
     </body>
 </html>
