2c1dd4c14978339902c105a39d03023965709af4
bertrandweb <feuille.bertrand@orange.fr>
2017-02-24 17:17:03 +0100

    Mise en place du php pour la vérif du formulaire. Il manque l'envoi de l'email

--- index.php
+++ index.php
@@ -1,192 +1,197 @@
 <?php
-session_start();
+
 ?>
 
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Bertrand Feuille | Webdéveloppeur-Webmaster</title>
+<head>
+    <meta charset="utf-8">
+    <title>Bertrand Feuille | Webdéveloppeur-Webmaster</title>
 
-        <!-- CSS -->
-        <link rel="stylesheet" href="./font/font-awesome/css/font-awesome.min.css">
-        <link rel="stylesheet" href="./css/hover.css">
-        <link rel="stylesheet" href="./css/animate.css">
-        <link rel="stylesheet" href="./css/normalized.css">
-        <link rel="stylesheet" href="./css/style.css">
-
-
-        <!-- FAVICON -->
-        <link rel="icon" type="image/png" href="./image/logofavicon.png" />
-        <!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="./image/favicon.ico" /><![endif]-->
+    <!-- CSS -->
+    <link rel="stylesheet" href="./font/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./css/hover.css">
+    <link rel="stylesheet" href="./css/animate.css">
+    <link rel="stylesheet" href="./css/normalized.css">
+    <link rel="stylesheet" href="./css/style.css">
 
 
-    </head>
-    <body>
-        <div class="page">
-        <header>
-          <div class="nom">
+    <!-- FAVICON -->
+    <link rel="icon" type="image/png" href="./image/logofavicon.png" />
+    <!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="./image/favicon.ico" /><![endif]-->
+
+
+</head>
+<body>
+<div class="page">
+    <header>
+        <div class="nom">
             <!-- Image + logo ici -->
             <img src="./image/logo2_site_cv.png" alt="logo de bertrand feuille">
             <p>Bertrand Feuille</p>
-          </div>
-            <nav id="header" class="navbar_header navbar navbar_default">
-              <ul>
+        </div>
+        <nav id="header" class="navbar_header navbar navbar_default">
+            <ul>
                 <li><i class="fa fa-bars" id="burger_menu" aria-hidden="true"></i></li>
                 <li><a class="hvr-underline-reveal scroll" href="#accueil">Accueil</a></li>
                 <li><a class="hvr-underline-reveal scroll" href="#presentation">Présentation</a></li>
                 <li><a class="hvr-underline-reveal scroll" href="#competence">Compétences</a></li>
                 <li><a class="hvr-underline-reveal scroll" href="#realisation">Réalisations</a></li>
                 <li><a class="hvr-underline-reveal scroll" href="#formulaire">Contact</a></li>
-              </ul>
-            </nav>
-
-        </header>
-        <!-- Menu responsive -->
-        <div class="menu_responsive">
-          <nav>
-            <ul>
-              <li><a class="scroll" href="#accueil">Accueil</a></li>
-              <li><a class="scroll" href="#presentation">Présentation</a></li>
-              <li><a class="scroll" href="#competence">Compétences</a></li>
-              <li><a class="scroll" href="#realisation">Réalisations</a></li>
-              <li><a class="scroll" href="#formulaire">Contact</a></li>
             </ul>
-          </nav>
-        </div>
-        <!-- Image de fond -->
-        <div id="accueil" class="hero">
-          <div class="filtre_hero">
+        </nav>
+
+    </header>
+    <!-- Menu responsive -->
+    <div class="menu_responsive">
+        <nav>
+            <ul>
+                <li><a class="scroll" href="#accueil">Accueil</a></li>
+                <li><a class="scroll" href="#presentation">Présentation</a></li>
+                <li><a class="scroll" href="#competence">Compétences</a></li>
+                <li><a class="scroll" href="#realisation">Réalisations</a></li>
+                <li><a class="scroll" href="#formulaire">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+    <!-- Image de fond -->
+    <div id="accueil" class="hero">
+        <div class="filtre_hero">
             <div class="bienvenue">
-              <h1>Site CV de Bertrand Feuille</h1>
-              <h2><span class="span_word">Webmaster</span> sur Rouen</h2>
-              <p>Passionné du Web et des nouvelles technologies depuis des années, je me présente aujourd’hui à vous en tant que <span>Webmaster</span>. Un métier qui me permet de mettre à profit mes compétences de développeur web, de graphiste et d’intégrateur.</p>
-              <a class="button scroll" href="#formulaire"><span>Contact</span></a>
+                <h1>Site CV de Bertrand Feuille</h1>
+                <h2><span class="span_word">Webmaster</span> sur Rouen</h2>
+                <p>Passionné du Web et des nouvelles technologies depuis des années, je me présente aujourd’hui à vous en tant que <span>Webmaster</span>. Un métier qui me permet de mettre à profit mes compétences de développeur web, de graphiste et d’intégrateur.</p>
+                <a class="button scroll" href="#formulaire"><span>Contact</span></a>
             </div>
-          </div>
         </div>
-        <!-- fin image de fond -->
-        <section id=presentation class="presentation_bloc">
-          <div class="wrapper">
+    </div>
+    <!-- fin image de fond -->
+    <section id=presentation class="presentation_bloc">
+        <div class="wrapper">
             <div class="photo"><img src="./image/logo2_site_cv.png" alt=""></div>
             <div class="presentation">
-              <h3>Présentation</h3>
-              <p><strong>Après un BTS en informatique m’ayant formé en programmation, réseaux, développement web, j’ai choisi de compléter mon profil technique avec des compétences en graphisme, communication web et référencement.</strong></p>
-              <p>La Formation Qualifiante Webmaster du lycée Saliège m’a donné les outils pour concevoir,
-              organiser, intégrer et animer un site web. J’ai pu mettre en application lors de mes différentes
-              expériences professionnelles en tant que webdesigner- intégrateur au sein d’une équipes. Je perfectionne chaque jour ces compétences que ce soit pour mes projets personnels ou pour les projets de mes clients en tant qu’auto-entrepreneur.</p>
+                <h3>Présentation</h3>
+                <p><strong>Après un BTS en informatique m’ayant formé en programmation, réseaux, développement web, j’ai choisi de compléter mon profil technique avec des compétences en graphisme, communication web et référencement.</strong></p>
+                <p>La Formation Qualifiante Webmaster du lycée Saliège m’a donné les outils pour concevoir,
+                    organiser, intégrer et animer un site web. J’ai pu mettre en application lors de mes différentes
+                    expériences professionnelles en tant que webdesigner- intégrateur au sein d’une équipes. Je perfectionne chaque jour ces compétences que ce soit pour mes projets personnels ou pour les projets de mes clients en tant qu’auto-entrepreneur.</p>
             </div>
 
-          </div>
-        </section>
-        <!-- bloc compétence -->
-        <section id="competence" class="competence"><h3>Compétences</h3>
-          <div class="wrapper">
+        </div>
+    </section>
+    <!-- bloc compétence -->
+    <section id="competence" class="competence"><h3>Compétences</h3>
+        <div class="wrapper">
             <div class="competence_bloc">
-              <p><a href="#">Développement</a></p>
-              <p><i class="fa fa-code fa-4x"></i></p>
-              <p>Utilisant les langages HTML 5, CSS3 et PHP depuis plus de 5 ans, je maîtrise également Javascript et jQuery ce qui me confère les connaissances nécessaires pour modifier les CMS rencontré ou m’adapter aux nouvelles techniques des projets réalisés</p>
+                <p><a href="#">Développement</a></p>
+                <p><i class="fa fa-code fa-4x"></i></p>
+                <p>Utilisant les langages HTML 5, CSS3 et PHP depuis plus de 5 ans, je maîtrise également Javascript et jQuery ce qui me confère les connaissances nécessaires pour modifier les CMS rencontré ou m’adapter aux nouvelles techniques des projets réalisés</p>
             </div>
             <div class="competence_bloc">
-              <p><a href="#">Graphisme</a></p>
-              <p><i class="fa fa-picture-o fa-4x"></i></p>
-              <p>Maitrisant Photoshop et Illustrator je réalise les visuels dont j’ai besoin, les maquettes et charte graphique de site web. J’utilise ma créativité pour créer des logos, flyers, et retouches d’image.</p>
+                <p><a href="#">Graphisme</a></p>
+                <p><i class="fa fa-picture-o fa-4x"></i></p>
+                <p>Maitrisant Photoshop et Illustrator je réalise les visuels dont j’ai besoin, les maquettes et charte graphique de site web. J’utilise ma créativité pour créer des logos, flyers, et retouches d’image.</p>
             </div>
             <div class="competence_bloc">
-              <p><a href="#">Responsive Design</a></p>
-              <p><i class="fa fa-mobile fa-4x"></i></p>
-              <p>Le trafic web sur smartphones et tablettes n’étant plus négligeable. J’utilise mes compétences en intégration et design pour réaliser des sites qui s’adaptent à toutes les tailles de support.</p>
+                <p><a href="#">Responsive Design</a></p>
+                <p><i class="fa fa-mobile fa-4x"></i></p>
+                <p>Le trafic web sur smartphones et tablettes n’étant plus négligeable. J’utilise mes compétences en intégration et design pour réaliser des sites qui s’adaptent à toutes les tailles de support.</p>
             </div>
             <div class="competence_bloc">
-              <p><a href="#">Communication</a></p>
-              <p><i class="fa  fa-comments-o fa-4x"></i></p>
-              <p>Afin d’assurer la visibilité d’un site internet, je sais mettre en place une stratégie de communication et d’acquisition de trafic en utilisant différents leviers tels que les réseaux sociaux et le référencement naturel.</p>
+                <p><a href="#">Communication</a></p>
+                <p><i class="fa  fa-comments-o fa-4x"></i></p>
+                <p>Afin d’assurer la visibilité d’un site internet, je sais mettre en place une stratégie de communication et d’acquisition de trafic en utilisant différents leviers tels que les réseaux sociaux et le référencement naturel.</p>
             </div>
             <div class="competence_bloc">
-              <p><a href="#">Communication</a></p>
-              <p><i class="fa  fa-comments-o fa-4x"></i></p>
-              <p>Afin d’assurer la visibilité d’un site internet, je sais mettre en place une stratégie de communication et d’acquisition de trafic en utilisant différents leviers tels que les réseaux sociaux et le référencement naturel.</p>
+                <p><a href="#">Communication</a></p>
+                <p><i class="fa  fa-comments-o fa-4x"></i></p>
+                <p>Afin d’assurer la visibilité d’un site internet, je sais mettre en place une stratégie de communication et d’acquisition de trafic en utilisant différents leviers tels que les réseaux sociaux et le référencement naturel.</p>
             </div>
             <div class="competence_bloc">
-              <p><a href="#">Communication</a></p>
-              <p><i class="fa  fa-comments-o fa-4x"></i></p>
-              <p>Afin d’assurer la visibilité d’un site internet, je sais mettre en place une stratégie de communication et d’acquisition de trafic en utilisant différents leviers tels que les réseaux sociaux et le référencement naturel.</p>
+                <p><a href="#">Communication</a></p>
+                <p><i class="fa  fa-comments-o fa-4x"></i></p>
+                <p>Afin d’assurer la visibilité d’un site internet, je sais mettre en place une stratégie de communication et d’acquisition de trafic en utilisant différents leviers tels que les réseaux sociaux et le référencement naturel.</p>
             </div>
-          </div>
-          <a class="button" href="./pdf/bertrand_feuille_cv.pdf"><span>Plus d'informations</span></a>
-        </section>
-        <!-- debut image background -->
-        <div class="hero2">
+        </div>
+        <a class="button" href="./pdf/bertrand_feuille_cv.pdf"><span>Plus d'informations</span></a>
+    </section>
+    <!-- debut image background -->
+    <div class="hero2">
         <div class="filtre_hero2">
         </div>
-        </div>
-        <!-- fin image background -->
-        <section id="realisation" class="realisation">
-          <div class="intro_realisation">
+    </div>
+    <!-- fin image background -->
+    <section id="realisation" class="realisation">
+        <div class="intro_realisation">
             <h3>Mes réalisations</h3>
-          </div>
-          <div class="wrapper">
+        </div>
+        <div class="wrapper">
             <div class="realisation_bloc">
-              <a href="http://ikedori.bertrandfeuille.fr" target=_blank><img src="./image/realisation3.jpg" alt="logo de ikedori"></a>
+                <a href="http://ikedori.bertrandfeuille.fr" target=_blank><img src="./image/realisation3.jpg" alt="logo de ikedori"></a>
             </div>
             <div class="realisation_bloc">
-              <a href="http://www.influenceursduweb.org/" target=_blank><img src="./image/realisation1.jpg" alt="logo des influenceurs du web"></a>
+                <a href="http://www.influenceursduweb.org/" target=_blank><img src="./image/realisation1.jpg" alt="logo des influenceurs du web"></a>
             </div>
             <div class="realisation_bloc">
-              <a href="http://www.webmaster-formation.fr" target=_blank><img src="./image/realisation2.jpg" alt="logo de ikedori"></a>
+                <a href="http://www.webmaster-formation.fr" target=_blank><img src="./image/realisation2.jpg" alt="logo de ikedori"></a>
             </div>
-          </div>
-        </section>
-        <!-- debut image background -->
-        <div class="hero3">
+        </div>
+    </section>
+    <!-- debut image background -->
+    <div class="hero3">
         <div class="filtre_hero2">
         </div>
-        </div>
-        <!-- fin image background -->
-        <!-- Section contact -->
-        <section id="formulaire" class="formulaire_contact">
-          <h3>Me contacter</h3>
-                <form id="form1" name="form1" action="contact.php" method="post">
-                    <p><input type="text" name="name" maxlength="50" value="" placeholder="Nom" value="">
-                    <input type="email" name="email" maxlength="50" value="" placeholder="Adresse e-mail"value=""></p>
-                    <textarea name="message"  rows="8" cols="80" placeholder="Message"></textarea>
-                    <p><input type="submit"  value="Envoyer"></p>
-                </form>
+    </div>
+    <!-- fin image background -->
+    <!-- Section contact -->
+    <section id="formulaire" class="formulaire_contact">
+        <h3>Me contacter</h3>
+        <form action="index.php#formulaire" method="post">
+            <p><input type="text" name="nom" maxlength="50" placeholder="Nom" >
+                <input type="email" name="email" maxlength="50" placeholder="Adresse e-mail"></p>
+            <textarea name="message"  rows="8" cols="80" placeholder="Message"></textarea>
+            <?php
 
-        </section>
-        <section>
-            <div class="wrapper_contact">
-              <div class="contact_adress">
+            require ('./php/validform.php');
+
+            ?>
+            <p><input type="submit" name="envoi"  value="Envoyer"></p>
+        </form>
+
+    </section>
+    <section>
+        <div class="wrapper_contact">
+            <div class="contact_adress">
                 <p><i class="fa  fa-phone fa-2x"></i></p>
                 <h4>Téléphone</h4>
                 <h5>06 26 49 48 49</h5>
-              </div>
-              <div class="contact_adress">
+            </div>
+            <div class="contact_adress">
                 <a href="./pdf/bertrand_feuille_cv.pdf"><img src="./image/logo_cv.jpg" alt=""></a>
-              </div>
-              <div class="contact_adress">
+            </div>
+            <div class="contact_adress">
                 <p><i class="fa  fa-envelope-o fa-2x"></i></p>
                 <h4>Email</h4>
                 <h5><a href="#">contact@bertrandfeuille.fr</a></h5>
-              </div>
             </div>
-            <div class="social_network">
-              <p>
+        </div>
+        <div class="social_network">
+            <p>
                 <a href="https://twitter.com/Bertrand_Web" target=_blank><i class="fa  fa-twitter fa-2x"></i></a>
                 <a href="https://www.linkedin.com/in/bertrand-feuille-6ba79aaa?authType=NAME_SEARCH&authToken=_4O6&locale=fr_FR&srchid=3901246541487256692129&srchindex=1&srchtotal=1&trk=vsrp_people_res_name&trkInfo=VSRPsearchId%3A3901246541487256692129%2CVSRPtargetId%3A390124654%2CVSRPcmpt%3Aprimary%2CVSRPnm%3Atrue%2CauthType%3ANAME_SEARCH" target=_blank><i id="linkedin" class="fa  fa-linkedin-square fa-2x"></i></a>
-              </p>
-            </div>
-        </section>
-
-        <footer>
-          <p>Copiright 2017 - <a href="#">Mentions légales</a></p>
-        </footer>
+            </p>
         </div>
+    </section>
 
-        <!-- JS -->
-        <script type="text/javascript" src="./js/jquery-3.1.1.js"></script>
-        <script type="text/javascript" src="./js/master.js"></script>
+    <footer>
+        <p>Copiright 2017 - <a href="#">Mentions légales</a></p>
+    </footer>
+</div>
 
-    </body>
+<!-- JS -->
+<script type="text/javascript" src="./js/jquery-3.1.1.js"></script>
+<script type="text/javascript" src="./js/master.js"></script>
+
+</body>
 </html>
---
+++ php/validform.php
@@ -0,0 +1,76 @@
+<?php
+
+
+    // if(isset($_POST['nom'], $_POST['email'], $_POST['message']))
+    //
+    // {
+    //     $_SESSION['verif']==false;
+    //     var_dump($_SESSION['verif']);
+    //     $TO = "contact@bertrandfeuille.fr";
+    //     $h  = "From: " . $TO;
+    //     $message = "";
+    //     while (list($key, $val) = each($_POST)) {
+    //         $message .= "$key : $val\n";
+    //     }
+    //     mail($TO, $message, $h);
+    //
+    //     Header("Location: http://localhost/sitecv/sitecv/index.php#formulaire");
+    // }
+    //
+    // else
+    // {
+    //     $_SESSION['verif']==true;
+    //     Header("Location: http://localhost/sitecv/sitecv/index.php#formulaire");
+    //
+    // }
+
+
+    // S'il y des données de postées
+    if ($_SERVER['REQUEST_METHOD']=='POST') {
+      // Code PHP pour traiter l'envoi de l'email
+
+      $nombreErreur = 0; // Variable qui compte le nombre d'erreur
+      // Définit toutes les erreurs possibles
+      if (!isset($_POST['email'])) { // Si la variable "email" du formulaire n'existe pas (il y a un problème)
+        $nombreErreur++; // On incrémente la variable qui compte les erreurs
+        $erreur1 = '<p>Il y a un problème avec la variable "email".</p>';
+      } else { // Sinon, cela signifie que la variable existe (c'est normal)
+        if (empty($_POST['email'])) { // Si la variable est vide
+          $nombreErreur++; // On incrémente la variable qui compte les erreurs
+          $erreur2 = '<p>Vous avez oublié de donner votre email.</p>';
+        } else {
+          if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $nombreErreur++; // On incrémente la variable qui compte les erreurs
+            $erreur3 = '<p>Cet email ne ressemble pas un email.</p>';
+          }
+        }
+      }
+
+      if (!isset($_POST['message'])) {
+        $nombreErreur++;
+        $erreur4 = '<p>Il y a un problème avec la variable "message".</p>';
+      } else {
+        if (empty($_POST['message'])) {
+          $nombreErreur++;
+          $erreur5 = '<p>Vous avez oublié de donner un message.</p>';
+        }
+      }    // (3) Ici, il sera possible d'ajouter plus tard un code pour vérifier un captcha anti-spam.
+
+      if ($nombreErreur==0) { // S'il n'y a pas d'erreur
+        // Ici il faut ajouter tout le code pour envoyer l'email.
+        // Dans le code présenté au chapitre précédent, cela signifie au code entre les commentaires (1) et (2).
+        echo '<div style="border:1px solid rgb(87, 181, 34); padding:5px;">';
+        echo '<p style="color:rgb(87, 181, 34);">Votre message a bien été envoyé';
+      } else { // S'il y a un moins une erreur
+
+        echo '<div style="border:1px solid #ff0000; padding:5px;">';
+        echo '<p style="color:#ff0000;">Désolé, il y a eu '.$nombreErreur.' erreur(s).</p>';
+        if (isset($erreur1)) echo '<p>'.$erreur1.'</p>';
+        if (isset($erreur2)) echo '<p>'.$erreur2.'</p>';
+        if (isset($erreur3)) echo '<p>'.$erreur3.'</p>';
+        if (isset($erreur4)) echo '<p>'.$erreur4.'</p>';
+        if (isset($erreur5)) echo '<p>'.$erreur5.'</p>';
+
+      }
+    }
+?>
